b9584a24e7a333c4eb08c8d5b4c92bf0abded51c
TLgaming294 <118128808+TLgaming294@users.noreply.github.com>
2022-12-29 17:46:34 +0100

    Add files via upload

---
+++ fragen.js
@@ -0,0 +1,8 @@
+const fragen = [];
+fragen[0] = "Hallo"
+fragen[1] = "Ich Bin du"
+fragen[2] = "Ich Bin dudelsack"
+fragen[3] = "wasser ist nass"
+fragen[4] = "regenbogen"
+fragen[5] = "blobfisch"
+fragen[6] = "hustensaft"
