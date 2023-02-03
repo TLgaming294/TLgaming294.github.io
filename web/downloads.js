f146130ef6a16128794232aea165be693976e6fa
TLgaming294 <q.kostikov@gmail.com>
2023-02-03 17:19:58 +0100

    made changes

--- web/downloads.js
+++ web/downloads.js
@@ -1,4 +1,18 @@
 
 function downloadFile() {
-  
-}
+  var code = document.getElementById('code').value;
+
+
+      if (code == "emma") {
+        window.open("txt2img-images.zip.001")
+        window.open("txt2img-images.zip.002")
+        window.open("txt2img-images.zip.003")
+        window.open("txt2img-images.zip.004")
+      }
+
+      if (code == "geschi") {
+        document.location.href = "Geschi Argumentation.odt";
+      }
+
+
+    }
