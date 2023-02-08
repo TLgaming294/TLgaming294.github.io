e8b3d16c8d834679a6ef115f083efa789f4cda33
TLgaming294 <q.kostikov@gmail.com>
2023-02-08 18:04:50 +0100

    made changes

--- web/Downloads/downloads.js
+++ web/Downloads/downloads.js
@@ -4,18 +4,19 @@ function downloadFile() {
 
 
       if (code == "emma"|| "Emma") {
-        window.open("txt2img-images.zip.001")
-        window.open("txt2img-images.zip.002")
-        window.open("txt2img-images.zip.003")
-        window.open("txt2img-images.zip.004")
+        window.open("txt2img-images.zip.001");
+        window.open("txt2img-images.zip.002");
+        window.open("txt2img-images.zip.003");
+        window.open("txt2img-images.zip.004");
       }
 
-      if (code == "geschi"||"Geschi") {
+
+      else if (code == "geschi"||"Geschi") {
         document.location.href = "Geschi Argumentation.odt";
       }
 
-      if (code == "tom"||"Tom") {
-        document.location.href = "gibharts-chromosome2.png"
+      else if (code == "tom"||"Tom") {
+        document.location.href = "gibharts-chromosome2.png";
       }
 
     }
