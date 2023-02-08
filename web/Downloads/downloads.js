226ed404a7fd4405799a3ea70d6073ec1c405e45
TLgaming294 <q.kostikov@gmail.com>
2023-02-08 17:48:53 +0100

    made changes

--- web/Downloads/downloads.js
+++ web/Downloads/downloads.js
@@ -3,16 +3,19 @@ function downloadFile() {
   var code = document.getElementById('code').value;
 
 
-      if (code == "emma") {
+      if (code == "emma"|| "Emma") {
         window.open("txt2img-images.zip.001")
         window.open("txt2img-images.zip.002")
         window.open("txt2img-images.zip.003")
         window.open("txt2img-images.zip.004")
       }
 
-      if (code == "geschi") {
+      if (code == "geschi"||"Geschi") {
         document.location.href = "Geschi Argumentation.odt";
       }
 
+      if (code == "tom"||"Tom") {
+        document.location.href = "gibharts-chromosome2.png"
+      }
 
     }
